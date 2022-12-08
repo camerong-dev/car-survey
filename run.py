0322114e6d12d885362c9d5e2e014a18cc2229db
Cameron <c.gourley03@gmail.com>
2022-12-08 15:56:06 +0000

    Add car info request

--- run.py
+++ run.py
@@ -27,13 +27,15 @@ def introduction():
     print("You'll us provide some personal details")
     print("Once filled out, you can then answer the questions which appear")
     print("Your results will then be stored on a spreadsheet")
-    print("You can then view your results once completed")
+    print("You can then view your results once completed\n")
 
     while True:
-        introduction_input = input("Please enter letter 'e' and press enter\n")
+        introduction_input = input(
+            "Please press the letter 'e' and press enter\n"
+         )
 
         if introduction_input_check(introduction_input):
-            print("Loading...")
+            print("\nLoading...")
             break
     print("------------------------------------------------------------------")
 
@@ -51,8 +53,8 @@ def introduction_input_check(values):
                 "Please enter 'e' and try again."
             )
 
-    except ValueError as x:
-        print(f"Whoops! Something went wrong. {x} \n")
+    except ValueError as emsg:
+        print(f"Whoops! Something went wrong. {emsg} \n")
         return False
 
     return True
@@ -62,16 +64,17 @@ def personal_information_name():
     """
     Users are requested to input their name
     """
-    print("Please enter your name, including your surname")
+    print("\nPlease enter your name, including your surname")
     print("Example - Jane Doe\n")
 
     while True:
+        global name_input
         name_input = input("Please enter your name here:\n")
 
         if name_check(name_input):
-            print(f"Thank you {name_input}!")
+            print(f"Thank you {name_input.title()}!")
+            print("---------------------------------------------------------")
             break
-        print("-----------------------------------------------------------")
 
     return name_input
 
@@ -81,12 +84,13 @@ def personal_information_age():
     Users are requested to input their age
     """
     while True:
+        global age_input
         age_input = input("Please enter your age here:\n")
 
         if age_check(age_input):
-            age_input = print("Thank you for telling us your age!")
+            print("Thank you for telling us your age!\n")
+            print("---------------------------------------------------------")
             break
-        print("-----------------------------------------------------------")
 
     return age_input
 
@@ -100,8 +104,8 @@ def name_check(values):
             raise ValueError(
                 "Please enter a name and try again.\nExample: Jane Doe"
             )
-    except ValueError as x:
-        print(f"Whoops! Something went wrong. {x} \n")
+    except ValueError as emsg:
+        print(f"Whoops! Something went wrong. {emsg} \n")
         return False
 
     return True
@@ -116,13 +120,92 @@ def age_check(values):
             raise ValueError(
                 "Please enter an age and try again."
             )
-    except ValueError as x:
-        print(f"Whoops! Something went wrong. {x} \n")
+
+    except ValueError as emsg:
+        print(f"Whoops! Something went wrong. {emsg} \n")
         return False
 
     return True
 
 
-introduction()
-personal_information_name()
-personal_information_age()
+def car_info_intro():
+    """
+    Introduction for the user
+    to provide basic info
+    of their car
+    """
+    
+    print("Thank you " + str(name_input).title() + " for your details.")
+    print(
+        "We are now going to ask some basic questions about your car"
+    )
+    car_info_make()
+
+
+def car_info_make():
+    """
+    Asks for user to input car manufacturer
+    """
+    while True:
+        global car_make
+        car_make = input("Please enter the car manufacter here:\n")
+
+        if car_make_check(car_make):
+            car_info_model()
+            break
+        return car_info_make()
+
+
+def car_info_model():
+    """
+    Asks user to input car model
+    """
+    while True:
+        global car_model
+        car_model = input("\nPlease enter the car model here:\n")
+
+        if car_model_check(car_model):
+            car_info_year()
+            break
+        return car_info_model()
+
+
+def car_info_year():
+    """
+    Asks user to input car year
+    """
+    while True:
+        global car_year
+        car_year = input("\nPlease enter the year of the car here:\n")
+
+        if car_year_check(car_year):
+            car_info_check()
+
+
+def car_make_check(values):
+    """
+    ValueError if user inputs numbers
+    """
+    try:
+        if values.isdigit():
+            raise ValueError(
+                "Please enter a manufacturer and try again.\nExample: VolksWagen"
+            )
+    except ValueError as emsg:
+        print(f"Whoops! Something went wrong. {emsg} \n")
+        return False
+
+    return True
+
+
+def main():
+    """
+    Contains all major functions
+    """
+    introduction()
+    personal_information_name()
+    personal_information_age()
+    car_info_intro()
+
+
+main()
