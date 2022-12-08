1da6494c6da3688cd5aa7e089e5d68d4ef9ae17b
Cameron <c.gourley03@gmail.com>
2022-12-08 14:09:01 +0000

    Add name and age input with validation

--- run.py
+++ run.py
@@ -23,10 +23,11 @@ def introduction():
     print("So everyone can have honest insight to these cars. \n")
 
     print("What happens:")
-    print("After finishing this introduction section you will read the rules")
-    print("Once you've read the rules, you'll then provide some personal info")
+    print("After finishing this introduction section")
+    print("You'll us provide some personal details")
     print("Once filled out, you can then answer the questions which appear")
-    print("Your results will then be stored on a spreadsheet\n")
+    print("Your results will then be stored on a spreadsheet")
+    print("You can then view your results once completed")
 
     while True:
         introduction_input = input("Please enter letter 'e' and press enter\n")
@@ -57,4 +58,71 @@ def introduction_input_check(values):
     return True
 
 
+def personal_information_name():
+    """
+    Users are requested to input their name
+    """
+    print("Please enter your name, including your surname")
+    print("Example - Jane Doe\n")
+
+    while True:
+        name_input = input("Please enter your name here:\n")
+
+        if name_check(name_input):
+            print(f"Thank you {name_input}!")
+            break
+        print("-----------------------------------------------------------")
+
+    return name_input
+
+
+def personal_information_age():
+    """
+    Users are requested to input their age
+    """
+    while True:
+        age_input = input("Please enter your age here:\n")
+
+        if age_check(age_input):
+            age_input = print("Thank you for telling us your age!")
+            break
+        print("-----------------------------------------------------------")
+
+    return age_input
+
+
+def name_check(values):
+    """
+    ValueError if user inputs numbers
+    """
+    try:
+        if values.isdigit():
+            raise ValueError(
+                "Please enter a name and try again.\nExample: Jane Doe"
+            )
+    except ValueError as x:
+        print(f"Whoops! Something went wrong. {x} \n")
+        return False
+
+    return True
+
+
+def age_check(values):
+    """
+    ValueError if user inputs letters
+    """
+    try:
+        if values.isalpha():
+            raise ValueError(
+                "Please enter an age and try again."
+            )
+    except ValueError as x:
+        print(f"Whoops! Something went wrong. {x} \n")
+        return False
+
+    return True
+
+
 introduction()
+personal_information_name()
+personal_information_age()
