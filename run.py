3548d11118de00e93b51c4acb52616ebf87c1da8
Cameron <c.gourley03@gmail.com>
2022-12-08 10:44:06 +0000

    Add introduction and validation for user input

--- run.py
+++ run.py
@@ -12,8 +12,52 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('car_survey')
 
-survey_answers = SHEET.worksheet('survey_answers')
 
-data = survey_answers.get_all_values()
+def introduction():
+    """
+    Introduces the user to the program.
+    A short welcome message appears, followed by how it works
+    """
+    print("Welcome to the Car Survey!")
+    print("This survey is collect as many different opinons on peoples cars")
+    print("So everyone can have honest insight to these cars. \n")
 
-print(data)
+    print("What happens:")
+    print("After finishing this introduction section you will read the rules")
+    print("Once you've read the rules, you'll then provide some personal info")
+    print("Once filled out, you can then answer the questions which appear")
+    print("Your results will then be stored on a spreadsheet")
+
+    while True:
+        introduction_input = input("Please enter letter 'e' and press enter\n")
+
+        if introduction_input_check(introduction_input):
+            print("Loading...")
+            break
+    print("------------------------------------------------------------------")
+
+    return introduction_input
+
+
+def introduction_input_check(values):
+    """
+    ValueError if input does not match letter 'e'
+    ValueError if input is more than one letter
+    """
+    if values != "e":
+        raise ValueError(
+            "Invalid letter, please enter 'e' and try again."
+        )
+        return False
+
+    if len(values) != 1:
+        raise ValueError(
+            "Too many characters, please enter 'e' and try again."
+        )
+        return False
+
+    else:
+        return True
+
+
+introduction()
